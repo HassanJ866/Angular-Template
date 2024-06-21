1b3c0becb2b0390f2f1d3ac8457b58ef65b8022d
Hassan Jaber <hassanjaber866@gmail.com>
2024-06-21 23:15:36 +0300

    Added api example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'angular-template';
+export class AppComponent implements OnInit {
+  ratings: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.fetchBanners();
+  }
+
+  fetchBanners(): void {
+    this.http.get<any[]>('http://127.0.0.1:8000/api/').subscribe(
+      (data) => {
+        this.ratings = data;
+        console.log('Results:', this.ratings);
+      },
+      (error) => {
+        console.error('Error fetching results', error);
+      }
+    );
+  }
 }
